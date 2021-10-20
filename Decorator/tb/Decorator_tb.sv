d36283aebd56f340f5b3e2075bac58c983e26df7
Scrawach <scrawach@gmail.com>
2021-10-20 13:19:30 +0300

    Added use decorator small example

--- Decorator/tb/Decorator_tb.sv
+++ Decorator/tb/Decorator_tb.sv
@@ -3,9 +3,11 @@ module Decorator_tb;
     initial begin
         Transaction trans = new(170);
         BadTransaction badTrans = new(trans);
+        BadTransaction notSoBadTrans = new(badTrans);
         
-        show(trans);    // display 170
-        show(badTrans); // display 171
+        show(trans);         // display 170
+        show(badTrans);      // display 171
+        show(notSoBadTrans); // display 170
     end
     
     function void show(Transaction trans);
