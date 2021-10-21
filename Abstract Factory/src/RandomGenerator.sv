6940ed5489412de7d786b5410c4ed164e46f4c59
Scrawach <scrawach@gmail.com>
2021-10-21 16:34:56 +0300

    Type missing fixed

--- Abstract Factory/src/RandomGenerator.sv
+++ Abstract Factory/src/RandomGenerator.sv
@@ -1,6 +1,6 @@
 class RandomGenerator implements Generator;
   virtual function Transaction getTransaction();
-    RandomTransaction trans = new();
+    DummyTransaction trans = new();
     assert (trans.randomize() ) else $error("%0t: [RANDOM GENERATOR] randomize failed!", $time);
     return trans;
   endfunction
